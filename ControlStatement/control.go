5d6c717981bacd6d3e87d966fc4571aa1f92a90f
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-05-28 00:55:23 +0530

    Switch case

--- ControlStatement/control.go
+++ ControlStatement/control.go
@@ -1,9 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+// import (
+// 	"fmt"
+// 	"math/rand"
+// )
 
 // func main () {
 //    hotelName := "The fucking hotel"
@@ -15,15 +15,28 @@ import (
 //    fmt.Println(roomsAvailable, "room available")
 // }
 
+// func main() {
+// 	var agePaul, ageJohn int = rand.Intn(110), rand.Intn(110)
+
+// 	fmt.Println("John is", ageJohn, "years old")
+//     fmt.Println("Paul  is", agePaul, "years old")
+
+// 	if ageJohn > agePaul {
+// 		fmt.Println("John is older than John")
+// 	} else {
+// 		fmt.Println("Paul is younger than you, or both have same age")
+// 	}
+// }
+
 func main() {
-	var agePaul, ageJohn int = rand.Intn(110), rand.Intn(110)
-
-	fmt.Println("John is", ageJohn, "years old")
-    fmt.Println("Paul  is", agePaul, "years old")
-
-	if ageJohn > agePaul {
-		fmt.Println("John is older than John")
-	} else {
-		fmt.Println("Paul is younger than you, or both have same age")
+	var gender = "female"
+   
+	switch gender {
+	case "male":
+		println("you male")
+	case "female":
+		println("your female")
+	default:
+		println("gay bsdk ka")
 	}
 }
