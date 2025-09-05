b264e65b56d7e9653ca47e9d3f603dfc03bdf523
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-09-05 18:05:05 +0530

    concurrency

---
+++ Go/concurrency/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+    count("sheep")
+	count("fish")
+}
+
+//synshornouss program 
+func count(thing string) {
+	for i := 1; true; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
