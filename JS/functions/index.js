c93c09ff456295bde2d9b90015f628b3d910d96a
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2026-06-28 00:13:33 +0530

    added js question rom lydia
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- JS/functions/index.js
+++ JS/functions/index.js
@@ -1,75 +1,85 @@
-// funnctions 
+// // funnctions 
 
-// functionn declaration 
+// // functionn declaration 
 
-function greet() {
-    console.log("heloo");
+// function greet() {
+//     console.log("heloo");
+// }
+
+// greet();
+
+// // Functions Expresion
+// // function stored inside a variable
+
+// const greet1 = function() {
+//     console.log("hello");
+// }
+
+// greet1();
+
+
+// // NAmed Function Expression
+// const greet2 = function sayHello() {
+//    console.log("hello");
+// }
+
+// greet2();
+
+
+// // Arrow functions
+
+// const greet3 = () => {
+
+// };
+
+// // Single Expression
+// const add = (a,b) => a + b;
+
+
+// // single Parameter
+
+// const square = (num) => num * num;
+
+
+
+// // Rest Parameters
+// // Collect remaining arguments
+
+// function sum(...nums) {
+//     console.log(nums)
+// }
+
+// sum(1,2,3);
+
+// // Return Values
+// // Functions can return anything
+
+// function add1(a, b) {
+//     return a + b;
+// }
+
+
+// // Return object
+
+// function createUser() {
+//     return {
+//         name: "Saurabh"
+//     };
+// }
+
+
+
+// // Return functions
+// function outer() {
+
+// }
+
+
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1);
 }
 
-greet();
-
-// Functions Expresion
-// function stored inside a variable
-
-const greet1 = function() {
-    console.log("hello");
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1);
 }
 
-greet1();
-
-
-// NAmed Function Expression
-const greet2 = function sayHello() {
-   console.log("hello");
-}
-
-greet2();
-
-
-// Arrow functions
-
-const greet3 = () => {
-
-};
-
-// Single Expression
-const add = (a,b) => a + b;
-
-
-// single Parameter
-
-const square = (num) => num * num;
-
-
-
-// Rest Parameters
-// Collect remaining arguments
-
-function sum(...nums) {
-    console.log(nums)
-}
-
-sum(1,2,3);
-
-// Return Values
-// Functions can return anything
-
-function add1(a, b) {
-    return a + b;
-}
-
-
-// Return object
-
-function createUser() {
-    return {
-        name: "Saurabh"
-    };
-}
-
-
-
-// Return functions
-function outer() {
-
-}
